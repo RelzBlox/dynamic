a5f4c200b302bde07250553b78119254603db769
RelzBlox <135495014+RelzBlox@users.noreply.github.com>
2023-06-04 14:00:57 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,6 @@
+local Data = {
+  { key = "TdGmSkK1t6", hwid = "aknwdlkafawf", valid_until = "2023-06-05 06:33:52" },
+  { key = "jISlCt4l84", hwid = "aknwajfaf", valid_until = "2023-06-05 06:43:25" },
+  { key = "t61kaAOShi", hwid = "aknw98w3093", valid_until = "2023-06-05 06:59:22" },
+}
+return Data
